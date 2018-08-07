a6ad2b45b888d7d1872e90d6e586d46760f2d1a7
Miloš Krstić <mrkrsticmilos@gmail.com>
2018-08-07 18:22:01 +0200

    selected players moved to service

--- src/app/aglf-components/team-overview/team-overview.component.ts
+++ src/app/aglf-components/team-overview/team-overview.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { PlayersService } from 'app/aglf-services/players.service';
+import { UserService } from 'app/aglf-services/user.service';
 import { Player, Position } from 'app/aglf-classes/player';
 
 @Component({
@@ -16,7 +17,7 @@ export class TeamOverviewComponent implements OnInit {
     players = [];
     selectedPlayers = new Array(15);
 
-    constructor(private playersService: PlayersService) { }
+    constructor(private playersService: PlayersService, private userService: UserService) { }
 
     ngOnInit() {
         this.playersService.getPlayers()
@@ -25,7 +26,14 @@ export class TeamOverviewComponent implements OnInit {
             )
             .subscribe(players => {
                 this.players = players;
-                //this.selectedPlayers = players;
+            });
+
+        this.userService.getSelectedPlayers()
+            .pipe(
+                takeUntil(this._destroyed$)
+            )
+            .subscribe(players => {
+                this.selectedPlayers = players;
             });
     }
 
@@ -36,78 +44,7 @@ export class TeamOverviewComponent implements OnInit {
 
     selectPlayerEventHandler(player: Player) {
         if (player instanceof Player) {
-            switch (player.position) {
-                case Position.GOALKEEPER:
-                    this.addGoalkeeper(player);
-                    break;
-
-                case Position.DEFENDER:
-                    this.addDefender(player);
-                    break;
-
-                case Position.MIDFILDER:
-                    this.addMidfielder(player);
-                    break;
-
-                case Position.STRIKER:
-                    this.addStriker(player);
-                    break;
-            }
+            this.userService.addPlayer(player);
         }
     }
-
-    addGoalkeeper(player: Player) {
-        if (this.selectedPlayers[0] && this.selectedPlayers[1]) {
-            console.log('cannot add any more gks');
-        } else if (this.selectedPlayers[0]) {
-            this.selectedPlayers[1] = new Player(player);
-        } else {
-            this.selectedPlayers[0] = new Player(player);
-        }
-    }
-
-    addDefender(player: Player) {
-        if (this.selectedPlayers[2] && this.selectedPlayers[3] && this.selectedPlayers[4] && this.selectedPlayers[5] && this.selectedPlayers[6]) {
-            console.log('cannot add any more dfs');
-        } else if (this.selectedPlayers[5]) {
-            this.selectedPlayers[6] = new Player(player);
-        } else if (this.selectedPlayers[4]) {
-            this.selectedPlayers[5] = new Player(player);
-        } else if (this.selectedPlayers[3]) {
-            this.selectedPlayers[4] = new Player(player);
-        } else if (this.selectedPlayers[2]) {
-            this.selectedPlayers[3] = new Player(player);
-        } else {
-            this.selectedPlayers[2] = new Player(player);
-        }
-    }
-
-    addMidfielder(player: Player) {
-        if (this.selectedPlayers[7] && this.selectedPlayers[8] && this.selectedPlayers[9] && this.selectedPlayers[10] && this.selectedPlayers[11]) {
-            console.log('cannot add any more mfs');
-        } else if (this.selectedPlayers[10]) {
-            this.selectedPlayers[11] = new Player(player);
-        } else if (this.selectedPlayers[9]) {
-            this.selectedPlayers[10] = new Player(player);
-        } else if (this.selectedPlayers[8]) {
-            this.selectedPlayers[9] = new Player(player);
-        } else if (this.selectedPlayers[7]) {
-            this.selectedPlayers[8] = new Player(player);
-        } else {
-            this.selectedPlayers[7] = new Player(player);
-        }
-    }
-
-    addStriker(player: Player) {
-        if (this.selectedPlayers[12] && this.selectedPlayers[13] && this.selectedPlayers[14]) {
-            console.log('cannot add any more sts');
-        } else if (this.selectedPlayers[13]) {
-            this.selectedPlayers[14] = new Player(player);
-        } else if (this.selectedPlayers[12]) {
-            this.selectedPlayers[13] = new Player(player);
-        } else {
-            this.selectedPlayers[12] = new Player(player);
-        }
-    }
-
 }
--- src/app/aglf-services/user.service.ts
+++ src/app/aglf-services/user.service.ts
@@ -1,9 +1,111 @@
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { Player, Position } from 'app/aglf-classes/player';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+    private _selectedPlayers$: BehaviorSubject<Player[]> = new BehaviorSubject<Player[]>(new Array(15));
+
+    constructor() { }
+
+    getSelectedPlayers(): Observable<Player[]> {
+        return this._selectedPlayers$.asObservable();
+    }
+
+    addPlayer(player: Player) {
+        if (this.checkIfPlayerExists(player)) {
+            console.log(player.fullName, ' is already in the team. ID =', player.id);
+        } else {
+            switch (player.position) {
+                case Position.GOALKEEPER:
+                    this.addGoalkeeper(player);
+                    break;
+
+                case Position.DEFENDER:
+                    this.addDefender(player);
+                    break;
+
+                case Position.MIDFILDER:
+                    this.addMidfielder(player);
+                    break;
+
+                case Position.STRIKER:
+                    this.addStriker(player);
+                    break;
+            }
+        }
+    }
+
+    checkIfPlayerExists(player: Player): boolean {
+        return this._selectedPlayers$.value.find(p => p ? p.id === player.id : false) ? true : false;
+    }
+
+    addGoalkeeper(player: Player) {
+        let selectedPlayers = this._selectedPlayers$.value;
+        if (selectedPlayers[0] && selectedPlayers[1]) {
+            console.log('cannot add any more gks');
+        } else if (selectedPlayers[0]) {
+            selectedPlayers[1] = new Player(player);
+        } else {
+            selectedPlayers[0] = new Player(player);
+        }
+
+        this._selectedPlayers$.next(selectedPlayers);
+    }
+
+    addDefender(player: Player) {
+        let selectedPlayers = this._selectedPlayers$.value;
+        if (selectedPlayers[2] && selectedPlayers[3] && selectedPlayers[4] && selectedPlayers[5] && selectedPlayers[6]) {
+            console.log('cannot add any more dfs');
+        } else if (selectedPlayers[5]) {
+            selectedPlayers[6] = new Player(player);
+        } else if (selectedPlayers[4]) {
+            selectedPlayers[5] = new Player(player);
+        } else if (selectedPlayers[3]) {
+            selectedPlayers[4] = new Player(player);
+        } else if (selectedPlayers[2]) {
+            selectedPlayers[3] = new Player(player);
+        } else {
+            selectedPlayers[2] = new Player(player);
+        }
+
+        this._selectedPlayers$.next(selectedPlayers);
+    }
+
+    addMidfielder(player: Player) {
+        let selectedPlayers = this._selectedPlayers$.value;
+        if (selectedPlayers[7] && selectedPlayers[8] && selectedPlayers[9] && selectedPlayers[10] && selectedPlayers[11]) {
+            console.log('cannot add any more mfs');
+        } else if (selectedPlayers[10]) {
+            selectedPlayers[11] = new Player(player);
+        } else if (selectedPlayers[9]) {
+            selectedPlayers[10] = new Player(player);
+        } else if (selectedPlayers[8]) {
+            selectedPlayers[9] = new Player(player);
+        } else if (selectedPlayers[7]) {
+            selectedPlayers[8] = new Player(player);
+        } else {
+            selectedPlayers[7] = new Player(player);
+        }
+
+        this._selectedPlayers$.next(selectedPlayers);
+    }
+
+    addStriker(player: Player) {
+        let selectedPlayers = this._selectedPlayers$.value;
+        if (selectedPlayers[12] && selectedPlayers[13] && selectedPlayers[14]) {
+            console.log('cannot add any more sts');
+        } else if (selectedPlayers[13]) {
+            selectedPlayers[14] = new Player(player);
+        } else if (selectedPlayers[12]) {
+            selectedPlayers[13] = new Player(player);
+        } else {
+            selectedPlayers[12] = new Player(player);
+        }
+
+        this._selectedPlayers$.next(selectedPlayers);
+    }
 }
