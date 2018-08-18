3870b921e61ec747aeee7e98edc1a5e4e7c65faa
gazivoda <gazivodai61@gmail.com>
2018-08-19 01:06:41 +0200

    Registration fixed

--- src/app/pages/content-pages/register/register-page.component.ts
+++ src/app/pages/content-pages/register/register-page.component.ts
@@ -18,13 +18,13 @@ export class RegisterPageComponent {
 
   onSubmit() {
     console.log('SUBMIT', this.registerForm.value.inputEmail, this.registerForm.value.inputPass);
-    this.authService.signupUser(this.registerForm.value.inputEmail, this.registerForm.value.inputPass).subscribe(val => {
-      console.log('&&&&&&& VAL', val);
+    this.authService.signupUser(this.registerForm.value.inputEmail, this.registerForm.value.inputPass).subscribe(() => {
     }, err => {
-      console.log('&&&&&&& ERR', err);
-      this.registerForm.reset();
-      this.authService.setToken(err.error.text);
-      this.router.navigate(['dashboard/dashboard1']);
+      if (err.status === 200) {
+        this.registerForm.reset();
+        this.authService.setToken(err.error.text);
+        this.router.navigate(['dashboard/dashboard1']);
+      }
     });
 
   }
