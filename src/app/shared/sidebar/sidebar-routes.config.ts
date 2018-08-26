c414981d60868b4af8630606b6efc5bad48016db
gazivoda <gazivodai61@gmail.com>
2018-08-26 23:55:48 +0200

    Navigation change

--- src/app/shared/sidebar/sidebar-routes.config.ts
+++ src/app/shared/sidebar/sidebar-routes.config.ts
@@ -2,7 +2,37 @@ import {RouteInfo} from './sidebar.metadata';
 
 //Sidebar menu Routes and data
 export const ROUTES: RouteInfo[] = [
-
+  {
+    path: '/dashboard/dashboard1',
+    title: 'Status',
+    icon: 'ft-home',
+    class: '',
+    badge: '',
+    badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1',
+    isExternalLink: false,
+    submenu: [
+      // {
+      //   path: '/dashboard/dashboard1',
+      //   title: 'Dashboard1',
+      //   icon: '',
+      //   class: '',
+      //   badge: '',
+      //   badgeClass: '',
+      //   isExternalLink: false,
+      //   submenu: []
+      // },
+      // {
+      //   path: '/dashboard/dashboard2',
+      //   title: 'Dashboard2',
+      //   icon: '',
+      //   class: '',
+      //   badge: '',
+      //   badgeClass: '',
+      //   isExternalLink: false,
+      //   submenu: []
+      // },
+    ]
+  },
   {
     path: '/team',
     title: 'My Team',
@@ -12,37 +42,6 @@ export const ROUTES: RouteInfo[] = [
     badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1',
     isExternalLink: false,
     submenu: []
-  },
-  {
-    path: '',
-    title: 'Dashboard',
-    icon: 'ft-home',
-    class: 'has-sub',
-    badge: '2',
-    badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1',
-    isExternalLink: false,
-    submenu: [
-      {
-        path: '/dashboard/dashboard1',
-        title: 'Dashboard1',
-        icon: '',
-        class: '',
-        badge: '',
-        badgeClass: '',
-        isExternalLink: false,
-        submenu: []
-      },
-      {
-        path: '/dashboard/dashboard2',
-        title: 'Dashboard2',
-        icon: '',
-        class: '',
-        badge: '',
-        badgeClass: '',
-        isExternalLink: false,
-        submenu: []
-      },
-    ]
   },
   // {
   //   path: '/colorpalettes',
