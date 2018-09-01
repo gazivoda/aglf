1b918cf9bcb7d7528f0e77357430a13a6740badb
gazivoda <gazivodai61@gmail.com>
2018-09-01 20:28:19 +0200

    Responsive design

--- src/app/aglf-components/team-overview/team-overview.component.ts
+++ src/app/aglf-components/team-overview/team-overview.component.ts
@@ -1,131 +1,131 @@
-import { Component, OnInit, Input, TemplateRef } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { PlayersService } from 'app/aglf-services/players.service';
-import { UserService } from 'app/aglf-services/user.service';
-import { Player, Position, PlayerData } from 'app/aglf-classes/player';
-import { FormGroup, FormControl } from '@angular/forms';
+import {Component, OnInit, TemplateRef} from '@angular/core';
+import {ReplaySubject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
+import {PlayersService} from 'app/aglf-services/players.service';
+import {UserService} from 'app/aglf-services/user.service';
+import {Player, PlayerData, Position} from 'app/aglf-classes/player';
+import {FormControl, FormGroup} from '@angular/forms';
 
-import { NgbModal, ModalDismissReasons, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-    selector: 'app-team-overview',
-    templateUrl: './team-overview.component.html',
-    styleUrls: ['./team-overview.component.scss']
+  selector: 'app-team-overview',
+  templateUrl: './team-overview.component.html',
+  styleUrls: ['./team-overview.component.scss']
 })
 export class TeamOverviewComponent implements OnInit {
 
-    private _destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+  private _destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-    players: Player[] = [];
-    selectedPlayers: Player[] = [];
-    budget: number;
+  players: Player[] = [];
+  selectedPlayers: Player[] = [];
+  budget: number;
 
-    position: Position | number = 0;
+  position: Position | number = 0;
 
-    selectedPlayer: Player = null;
+  selectedPlayer: Player = null;
 
-    playerRoleForm: FormGroup;
+  playerRoleForm: FormGroup;
 
-    constructor(private playersService: PlayersService, private userService: UserService, private modalService: NgbModal) {
-        this.playerRoleForm = new FormGroup({
-            id: new FormControl(''),
-            active: new FormControl(false),
-            captain: new FormControl(false),
-            viceCaptain: new FormControl(false)
-        });
-    }
+  constructor(private playersService: PlayersService, private userService: UserService, private modalService: NgbModal) {
+    this.playerRoleForm = new FormGroup({
+      id: new FormControl(''),
+      active: new FormControl(false),
+      captain: new FormControl(false),
+      viceCaptain: new FormControl(false)
+    });
+  }
 
-    ngOnInit() {
-        this.userService.getUserDetails()
-            .subscribe((data: any) => {
-                let selectedPlayers = data.players;
-                this.userService.setSelectedPlayers(selectedPlayers);
-            });
+  ngOnInit() {
+    this.userService.getUserDetails()
+      .subscribe((data: any) => {
+        let selectedPlayers = data.players;
+        this.userService.setSelectedPlayers(selectedPlayers);
+      });
 
-        this.playersService.getPlayers()
-            .pipe(
-                takeUntil(this._destroyed$)
-            )
-            .subscribe((players: Player[]) => {
-                this.players = players;
+    this.playersService.getPlayers()
+      .pipe(
+        takeUntil(this._destroyed$)
+      )
+      .subscribe((players: Player[]) => {
+        this.players = players;
 
-                if (players.length > 0) {
-                    let prices = players.map(player => player.price).filter(p => p !== null);
-                    console.log(Math.max(...prices), Math.min(...prices));
-                }
-            });
-
-        this.userService.getSelectedPlayers()
-            .pipe(
-                takeUntil(this._destroyed$)
-            )
-            .subscribe((selectedPlayers: Player[]) => {
-                this.selectedPlayers = selectedPlayers;
-            });
-
-        this.userService.getBudget()
-            .pipe(
-                takeUntil(this._destroyed$)
-            )
-            .subscribe((budget: number) => {
-                this.budget = budget;
-            });
-
-        this.playerRoleForm.controls.captain.valueChanges
-            .pipe(
-                takeUntil(this._destroyed$)
-            )
-            .subscribe((isCaptain: boolean) => {
-                if (isCaptain) {
-                    this.playerRoleForm.controls.viceCaptain.setValue(false);
-                }
-            });
-        this.playerRoleForm.controls.viceCaptain.valueChanges
-            .pipe(
-                takeUntil(this._destroyed$)
-            )
-            .subscribe((isViceCaptain: boolean) => {
-                if (isViceCaptain) {
-                    this.playerRoleForm.controls.captain.setValue(false);
-                }
-            });
-    }
-
-    ngOnDestroy() {
-        this._destroyed$.next(true);
-        this._destroyed$.complete();
-    }
-
-    selectPlayerEventHandler(player: Player) {
-        if (player instanceof Player) {
-            this.userService.addPlayer(player, true);
+        if (players.length > 0) {
+          let prices = players.map(player => player.price).filter(p => p !== null);
+          console.log(Math.max(...prices), Math.min(...prices));
         }
-    }
+      });
 
-    removePlayerEventHandler(player: Player) {
-        this.userService.removePlayer(player);
-    }
+    this.userService.getSelectedPlayers()
+      .pipe(
+        takeUntil(this._destroyed$)
+      )
+      .subscribe((selectedPlayers: Player[]) => {
+        this.selectedPlayers = selectedPlayers;
+      });
 
-    selectPositionEventHandler(position: Position) {
-        this.position = position;
-    }
+    this.userService.getBudget()
+      .pipe(
+        takeUntil(this._destroyed$)
+      )
+      .subscribe((budget: number) => {
+        this.budget = budget;
+      });
 
-    openPlayerModalEventHandler(player: Player, content: TemplateRef<NgbModal>) {
-        this.selectedPlayer = player;
-        this.playerRoleForm.controls.id.setValue(player.id);
-        this.playerRoleForm.controls.active.setValue(player.active);
-        this.playerRoleForm.controls.captain.setValue(player.captain);
-        this.playerRoleForm.controls.viceCaptain.setValue(player.viceCaptain);
-        this.open(content);
-    }
+    this.playerRoleForm.controls.captain.valueChanges
+      .pipe(
+        takeUntil(this._destroyed$)
+      )
+      .subscribe((isCaptain: boolean) => {
+        if (isCaptain) {
+          this.playerRoleForm.controls.viceCaptain.setValue(false);
+        }
+      });
+    this.playerRoleForm.controls.viceCaptain.valueChanges
+      .pipe(
+        takeUntil(this._destroyed$)
+      )
+      .subscribe((isViceCaptain: boolean) => {
+        if (isViceCaptain) {
+          this.playerRoleForm.controls.captain.setValue(false);
+        }
+      });
+  }
 
-    open(content) {
-        this.modalService.open(content).result.then((result) => {
-            let playerData: PlayerData = <PlayerData>this.playerRoleForm.value;
-            this.userService.updatePlayerData(playerData);
-        }, (reason) => {
-            console.log('modal closed');
-        });
+  ngOnDestroy() {
+    this._destroyed$.next(true);
+    this._destroyed$.complete();
+  }
+
+  selectPlayerEventHandler(player: Player) {
+    if (player instanceof Player) {
+      this.userService.addPlayer(player, true);
     }
+  }
+
+  removePlayerEventHandler(player: Player) {
+    this.userService.removePlayer(player);
+  }
+
+  selectPositionEventHandler(position: Position) {
+    this.position = position;
+  }
+
+  openPlayerModalEventHandler(player: Player, content: TemplateRef<NgbModal>) {
+    this.selectedPlayer = player;
+    this.playerRoleForm.controls.id.setValue(player.id);
+    this.playerRoleForm.controls.active.setValue(player.active);
+    this.playerRoleForm.controls.captain.setValue(player.captain);
+    this.playerRoleForm.controls.viceCaptain.setValue(player.viceCaptain);
+    this.open(content);
+  }
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      let playerData: PlayerData = <PlayerData>this.playerRoleForm.value;
+      this.userService.updatePlayerData(playerData);
+    }, (reason) => {
+      console.log('modal closed');
+    });
+  }
 }
